7e0e60405edd27f26d2c24cc7bc549156fa2853c
Liipka <michallipka1996@o2.pl>
2024-08-30 17:39:33 +0200

    addinng TVshow details feature and movies slider

--- js/script.js
+++ js/script.js
@@ -82,13 +82,15 @@ const displayMovieDetails = async () => {
 
   console.log(movie);
 
+  displayBackgroundImage("movie", movie.backdrop_path);
+
   const movieDetailsDiv = `
         <div class="details-top">
           <div>
             <img
             src=${
               movie.poster_path
-                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
                 : "images/no-image.jpg"
             }
               class="card-img-top"
@@ -143,6 +145,139 @@ const displayMovieDetails = async () => {
     .insertAdjacentHTML("afterbegin", movieDetailsDiv);
 };
 
+const displayShowDetails = async () => {
+  const showID = document.location.search.split("=")[1];
+  const show = await fetchAPIData(`tv/${showID}`);
+
+  displayBackgroundImage("tvShow", show.backdrop_path);
+
+  const showDetailsDiv = `
+  <div class="details-top">
+  <div>
+    <img
+      src=${
+        show.poster_path
+          ? `https://image.tmdb.org/t/p/w500/${show.poster_path}`
+          : "images/no-image.jpg"
+      }
+      class="card-img-top"
+      alt="${show.name}"
+    />
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Air Date: ${show.first_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+    ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit Show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li>
+      <span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+      }
+    </li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies
+    .map((company) => `<span>${company.name}</span>`)
+    .join(", ")}</div>
+</div>
+    `;
+
+  document
+    .querySelector("#show-details")
+    .insertAdjacentHTML("afterbegin", showDetailsDiv);
+};
+
+const displayBackgroundImage = (type, backgroundPath) => {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.15";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
+};
+
+const displaySlider = async () => {
+  const { results } = await fetchAPIData("movie/now_playing");
+  const wrapper = document.querySelector(".swiper-wrapper");
+
+  results.forEach((movie) => {
+    const slide = `
+    <div class="swiper-slide">
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+      movie.title
+    }" />
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+            1
+          )} / 10
+        </h4>
+    </div>   
+    `;
+    wrapper.insertAdjacentHTML("afterbegin", slide);
+
+    initSwiper();
+  });
+};
+
+const initSwiper = () => {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
+
 const fetchAPIData = async (endpoint) => {
   const API_KEY = "c9293e4ce5fe1fce429e0527afea0e25";
   const API_URL = "https://api.themoviedb.org/3/";
@@ -173,6 +308,7 @@ const init = () => {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displaySlider();
       displayPopularMovies();
       break;
     case "/shows.html":
@@ -182,7 +318,7 @@ const init = () => {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
